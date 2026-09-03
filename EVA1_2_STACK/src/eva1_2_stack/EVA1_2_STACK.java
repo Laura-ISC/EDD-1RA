344ef5f40674b64a579dead65469e4ae3e238cb1
Laura-ISC <laujocelyn31@gmail.com>
2026-09-03 16:48:52 -0600

    Add files via upload

---
+++ EVA1_2_STACK/src/eva1_2_stack/EVA1_2_STACK.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_stack;
+
+/**
+ *
+ * @author laura
+ */
+public class EVA1_2_STACK {
+   public static void main(String[] args) {
+      System.out.println("Inicia main");
+      A();
+      System.out.println("Termina main");
+   }
+   public static void A(){
+      System.out.println("Inicia A()");
+      B();
+      System.out.println("Termina A()");
+   }
+   public static void B(){
+      System.out.println("Inicia B()");
+      int i = 5;
+      double D = 5.5;
+      System.out.println("Termina B()");
+   }
+}
