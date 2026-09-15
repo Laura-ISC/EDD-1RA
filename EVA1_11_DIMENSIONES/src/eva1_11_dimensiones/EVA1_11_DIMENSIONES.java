2cdbd19f55ddf4c1b0ac3e21933908317fd8e03d
Laura-ISC <laujocelyn31@gmail.com>
2026-09-15 16:51:45 -0600

    Add files via upload

---
+++ EVA1_11_DIMENSIONES/src/eva1_11_dimensiones/EVA1_11_DIMENSIONES.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_11_dimensiones;
+
+/**
+ *
+ * @author laura
+ */
+public class EVA1_11_DIMENSIONES {
+
+   public static void main(String[] args) {
+      int[][] matriz = new int [3][];
+      //cada fila tendrá diferente cantidad de columnas
+      matriz[0] = new int[5];
+      matriz[1] = new int[10];
+      matriz[2] = new int[3];
+      for (int i = 0; i < matriz.length; i++) {
+         for (int j = 0; j < matriz.length; j++) {
+            matriz[i][j] = (int)(Math.random() * 100);
+         }
+      }
+      for (int i = 0; i < matriz.length; i++) {
+         for (int j = 0; j < matriz[i].length; j++) {
+            System.out.print("[" + matriz[i][j] + "]");
+         }
+         System.out.println("");
+      }
+   }
+   
+}
